4cabe70ac19f933992846b39574128026808c3c7
Kuuw <67199397+Kuuw@users.noreply.github.com>
2024-12-12 02:07:45 +0300

    Use products component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Products from './components/Products';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Products />
   </StrictMode>,
-)
+);
